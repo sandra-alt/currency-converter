54f4048276ddff205098dc850fa2045a9e9a268d
Oleksandra <oleksandra.revizina@gmail.com>
2024-12-12 12:34:34 +0100

    feat: set up an empty project

---
+++ currency-converter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  currency-converter
+//
+//  Created by  Oleksandra on 12.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
