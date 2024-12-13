41a841846e08715a6947d0cea555886852072586
Oleksandra <oleksandra.revizina@gmail.com>
2024-12-13 13:07:08 +0100

    feat: create UI components
    - Added ConverterViewController
    - Set up ConverterTileView
    - Removed unused ViewController

---
+++ currency-converter/ConverterFlow/ConverterTileView.swift
@@ -0,0 +1,157 @@
+//
+//  ConverterTileView.swift
+//  currency-converter
+//
+//  Created by  Oleksandra on 13.12.2024.
+//
+
+import Combine
+import UIKit
+
+class ConverterTileView: UIView {
+    
+    // MARK: - Layout properties
+    private struct Layout {
+        static let spacing: CGFloat = 16.0
+        static let horizontalSpacing: CGFloat = 24.0
+        static let cornerRadius: CGFloat = 12.0
+        
+        static let shadowRadius: CGFloat = 4.0
+        static let shadowOpacity: Float = 0.1
+        static let shadowOffset = CGSize(width: 0, height: 2.0)
+        static let shadowColor = UIColor.black.cgColor
+        
+        static let textColor = UIColor.black
+        static let buttonTextColor = UIColor.black
+    }
+    
+    // MARK: - UI Components
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    private lazy var tileView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = Layout.cornerRadius
+        view.layer.shadowColor = Layout.shadowColor
+        view.layer.shadowOpacity = Layout.shadowOpacity
+        view.layer.shadowOffset = Layout.shadowOffset
+        view.layer.shadowRadius = Layout.shadowRadius
+        return view
+    }()
+    
+    private lazy var vStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.alignment = .fill
+        view.distribution = .fill
+        view.spacing = Layout.spacing
+        return view
+    }()
+    
+    private lazy var fromHStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.alignment = .fill
+        view.distribution = .fill
+        view.spacing = Layout.horizontalSpacing
+        return view
+    }()
+    
+    private lazy var fromAmountTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter amount"
+        textField.textColor = Layout.textColor
+        textField.backgroundColor = .clear
+        return textField
+    }()
+    
+    private lazy var fromCurrencyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PLN", for: .normal)
+        button.setTitleColor(Layout.buttonTextColor, for: .normal)
+        button.contentHorizontalAlignment = .trailing
+        button.setContentCompressionResistancePriority(.required, for: .horizontal)
+        return button
+    }()
+    
+    private lazy var separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private lazy var toHStack: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.alignment = .fill
+        view.distribution = .fill
+        view.spacing = Layout.horizontalSpacing
+        return view
+    }()
+    
+    private lazy var toAmmountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0.00"
+        label.textColor = Layout.textColor
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var toCurrencyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("JPY", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.contentHorizontalAlignment = .trailing
+        button.setContentCompressionResistancePriority(.required, for: .horizontal)
+        return button
+    }()
+    
+    // MARK: - Variables
+    private var cancellables: Set<AnyCancellable> = []
+    
+    // MARK: - Initialization
+    init() {
+        super.init(frame: UIScreen.main.bounds)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: - View Setup
+    private func setupView() {
+        //
+        [fromAmountTextField, fromCurrencyButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            fromHStack.addArrangedSubview($0)
+        }
+        
+        [toAmmountLabel, toCurrencyButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            toHStack.addArrangedSubview($0)
+        }
+        
+        separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        [fromHStack, separatorView, toHStack].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            vStack.addArrangedSubview($0)
+        }
+        
+        tileView.addSubview(vStack)
+        vStack.bindFrameToSuperview(margin: Layout.spacing)
+        
+        containerView.addSubview(tileView)
+        tileView.bindFrameToSuperview()
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(containerView)
+        containerView.bindFrameToSuperview()
+    }
+}
---
+++ currency-converter/ConverterFlow/ConverterViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ConverterViewController.swift
+//  currency-converter
+//
+//  Created by  Oleksandra on 12.12.2024.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+    
+    // MARK: - Layout
+    private struct Layout {
+        static let topSpace: CGFloat = 48.0
+        static let margin: CGFloat = 24.0
+    }
+    
+    // MARK: - Properties
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    // MARK: - UI Components
+    private lazy var tileView: ConverterTileView = {
+        ConverterTileView()
+    }()
+    
+    // MARK: - Initialization
+    init(title: String) {
+        super.init(nibName: nil, bundle: nil)
+        
+        // Setup a title
+        self.title = title
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup views
+        setupScrollView()
+        setupUI()
+    }
+    
+    // MARK: - Scroll View Setup
+    private func setupScrollView() {
+        // Add scrollView to the main view
+        view.addSubview(scrollView)
+        
+        // Enable Auto Layout for the scrollView and pin the scrollView to the edges of the view
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.bindFrameToSuperview()
+        
+        // Add a content view inside the scroll view
+        scrollView.addSubview(contentView)
+        
+        // Enable Auto Layout for the contentView and pin the contentView to the scrollView's edges
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.bindFrameToSuperview()
+        
+        // Match contentView width and height for horizontal and vertical scrolling
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        view.backgroundColor = .systemGray4
+        
+        contentView.addSubview(tileView)
+        tileView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tileView.bindFrameToSuperview(top: Layout.topSpace, leading: Layout.margin, trailing: Layout.margin)
+    }
+}
